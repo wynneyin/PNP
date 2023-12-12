46a78cd2cd1395165576069d807ca6f5665b3838
jizhuoran <jizhuoran.work@gmail.com>
2023-12-12 11:36:56 +0000

    move parameter to templete

--- aten/src/ATen/native/biginteger/cuda/sppark-ntt/parameters/parameters.cuh
+++ aten/src/ATen/native/biginteger/cuda/sppark-ntt/parameters/parameters.cuh
@@ -2,12 +2,24 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <ATen/native/cuda/thread_constants.h>
 #include "ATen/native/biginteger/cuda/sppark-util/gpu_t.cuh"
-#include "bls12_381.h"
 #include "ATen/native/biginteger/cuda/CurveDef.cuh"
 #include "gen_twiddles.cuh"
 
+
+template <typename T>
+struct NTTHyperParam;
+#include "alt_bn128.h"
+#include "bls12_377.h"
+#include "bls12_381.h"
+
+
+
 namespace at { 
 namespace native {
+
+// General template (undefined)
+template <typename T>
+struct Constants;
 
 template <typename fr_t>
 void NTTParameters(bool inverse, int id, fr_t* data_ptr)
@@ -18,19 +30,19 @@ void NTTParameters(bool inverse, int id, fr_t* data_ptr)
     
     const size_t partial_sz = WINDOW_NUM * WINDOW_SIZE;
 
-    const size_t inverse_sz = S + 1;
+    const size_t inverse_sz = NTTHyperParam<fr_t>::S + 1;
     
 
     fr_t* Inverse_roots_of_unity = (fr_t*)gpu.Dmalloc(inverse_sz * sizeof(fr_t));
     
-    cudaMemcpyAsync(Inverse_roots_of_unity, inverse_roots_of_unity, 
+    cudaMemcpyAsync(Inverse_roots_of_unity, NTTHyperParam<fr_t>::inverse_roots_of_unity.data(), 
                     inverse_sz * sizeof(fr_t), cudaMemcpyHostToDevice,
                     gpu);
     C10_CUDA_KERNEL_LAUNCH_CHECK();
 
     fr_t* Forward_roots_of_unity = (fr_t*)gpu.Dmalloc(inverse_sz * sizeof(fr_t));
     
-    cudaMemcpyAsync(Forward_roots_of_unity, forward_roots_of_unity, 
+    cudaMemcpyAsync(Forward_roots_of_unity, NTTHyperParam<fr_t>::forward_roots_of_unity.data(), 
                     inverse_sz * sizeof(fr_t), cudaMemcpyHostToDevice,
                     gpu);
     C10_CUDA_KERNEL_LAUNCH_CHECK();
@@ -46,14 +58,14 @@ void NTTParameters(bool inverse, int id, fr_t* data_ptr)
 
     fr_t* Group_gen_inverse = (fr_t*)gpu.Dmalloc(sizeof(fr_t));
     
-    cudaMemcpyAsync(Group_gen_inverse, group_gen_inverse, 
+    cudaMemcpyAsync(Group_gen_inverse, NTTHyperParam<fr_t>::group_gen_inverse.data(), 
                         sizeof(fr_t), cudaMemcpyHostToDevice,
                         gpu);
     C10_CUDA_KERNEL_LAUNCH_CHECK();
 
     fr_t* Group_gen = (fr_t*)gpu.Dmalloc(sizeof(fr_t));
     
-    cudaMemcpyAsync(Group_gen, group_gen, 
+    cudaMemcpyAsync(Group_gen, NTTHyperParam<fr_t>::group_gen.data(), 
                         sizeof(fr_t), cudaMemcpyHostToDevice,
                         gpu);
     C10_CUDA_KERNEL_LAUNCH_CHECK();
@@ -79,7 +91,7 @@ void NTTParameters(bool inverse, int id, fr_t* data_ptr)
     generate_radixX_twiddles_X<<<16, 256, 0, gpu>>>(data_ptr + 2 * partial_sz + blob_sz + 64*64 + 4096*64 + 128*128, 256, roots+16);
     generate_radixX_twiddles_X<<<16, 512, 0, gpu>>>(data_ptr + 2 * partial_sz + blob_sz + 64*64 + 4096*64 + 128*128 + 256*256, 512, roots+18);
     
-    cudaMemcpyAsync(data_ptr + 2 * partial_sz + blob_sz + 64*64 + 4096*64 + 128*128 + 256*256 + 512*512, domain_size_inverse, 
+    cudaMemcpyAsync(data_ptr + 2 * partial_sz + blob_sz + 64*64 + 4096*64 + 128*128 + 256*256 + 512*512, NTTHyperParam<fr_t>::domain_size_inverse.data(), 
                             inverse_sz * sizeof(fr_t), cudaMemcpyHostToDevice,
                             gpu);
     C10_CUDA_KERNEL_LAUNCH_CHECK();

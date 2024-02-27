08e1c680d17242eef74373992966e5d004e8e10a
wynneyin <2830851361@qq.com>
2024-02-27 11:00:20 +0000

    Added NTT as well as cuda version of addition, subtraction and multiplication of unit tests, removed a.py

--- torch/testing/_internal/zkp_test_lists.py
+++ torch/testing/_internal/zkp_test_lists.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 import unittest
 import random
 
@@ -137,14 +138,29 @@ class CustomTestCase(unittest.TestCase):
                 if base1 < self.p[type_element] and base2 < self.p[type_element]:
                     break  # Exit loop if conditions are met
 
-            base1 = self.compute_base_mod(t1, self.modlist[type_element])
-            base2 = self.compute_base_mod(t2, self.modlist[type_element])
-            r1 = F.to_mont(t1)
-            r2 = F.to_mont(t2)
-            self.assertEqual(
-                (base1 + base2) % self.modlist[type_element],
-                self.compute_mont(F.add_mod(r1, r2)),
-            )
+            t2 = torch.tensor(random_array_2, dtype=type_element)
+            devices=['cuda','cpu']
+            for device in devices:
+
+                base1 = self.compute_base_mod(t1, self.modlist[type_element])
+                base2 = self.compute_base_mod(t2, self.modlist[type_element])
+
+                if device=='cuda':
+                    t1.to("cuda")
+                    t2.to("cuda")
+                    r1 = F.to_mont(t1)
+                    r2 = F.to_mont(t2)
+                    self.assertEqual(
+                        (base1 + base2) % self.modlist[type_element],
+                        self.compute_mont(F.add_mod(r1, r2)),
+                    )
+                else:
+                    r1 = F.to_mont(t1)
+                    r2 = F.to_mont(t2)
+                    self.assertEqual(
+                        (base1 + base2) % self.modlist[type_element],
+                        self.compute_mont(F.add_mod(r1, r2)),
+                    )
 
     def test_sub(self):
         min_dimension = 4
@@ -171,14 +187,29 @@ class CustomTestCase(unittest.TestCase):
 
                 if base1 < self.p[type_element] and base2 < self.p[type_element]:
                     break  # Exit loop if conditions are met
-            base1 = self.compute_base_mod(t1, self.modlist[type_element])
-            base2 = self.compute_base_mod(t2, self.modlist[type_element])
-            r1 = F.to_mont(t1)
-            r2 = F.to_mont(t2)
-            self.assertEqual(
-                (base1 - base2) % self.modlist[type_element],
-                self.compute_mont(F.sub_mod(r1, r2)),
-            )
+            t2 = torch.tensor(random_array_2, dtype=type_element)
+            devices=['cuda','cpu']
+            for device in devices:
+
+                base1 = self.compute_base_mod(t1, self.modlist[type_element])
+                base2 = self.compute_base_mod(t2, self.modlist[type_element])
+
+                if device=='cuda':
+                    t1.to("cuda")
+                    t2.to("cuda")
+                    r1 = F.to_mont(t1)
+                    r2 = F.to_mont(t2)
+                    self.assertEqual(
+                        (base1 + base2) % self.modlist[type_element],
+                        self.compute_mont(F.add_mod(r1, r2)),
+                    )
+                else:
+                    r1 = F.to_mont(t1)
+                    r2 = F.to_mont(t2)
+                    self.assertEqual(
+                        (base1 - base2) % self.modlist[type_element],
+                        self.compute_mont(F.sub_mod(r1, r2)),
+                    )
 
     def test_mul(self):
         min_dimension = 4
@@ -206,32 +237,86 @@ class CustomTestCase(unittest.TestCase):
                 if base1 < self.p[type_element] and base2 < self.p[type_element]:
                     break  # Exit loop if conditions are met
             t2 = torch.tensor(random_array_2, dtype=type_element)
+            devices=['cuda','cpu']
             base1 = self.compute_base(t1)
             base2 = self.compute_base(t2)
-            r1 = F.to_mont(t1)
-            r2 = F.to_mont(t2)
-            self.assertEqual(
-                ((base1 * base2) << 256) % self.modlist[type_element],
-                ((self.compute_mont(F.mul_mod(r1, r2)))) % self.modlist[type_element],
-            )
+            for device in devices:
+                if device=='cuda':
+                    r1 = F.to_mont(t1)
+                    r2 = F.to_mont(t2)
+                    r1.to("cuda")
+                    r2.to("cuda")
+                    self.assertEqual(
+                        ((base1 * base2) << 256) % self.modlist[type_element],
+                        ((self.compute_mont(F.mul_mod(r1, r2)))) % self.modlist[type_element],
+                    )
+                else:
+                    r1 = F.to_mont(t1)
+                    r2 = F.to_mont(t2)
+                    self.assertEqual(
+                        ((base1 * base2) << 256) % self.modlist[type_element],
+                        ((self.compute_mont(F.mul_mod(r1, r2)))) % self.modlist[type_element],
+                    )
+  ########### Check the correctness of ntt and intt with gpu ###########  
+    def test_ntt(self):       
+        domain_size = 32
+        nttclass = nn.Ntt(domain_size, torch.BLS12_377_Fr_G1_Mont)
+        inttclass = nn.Intt(domain_size, torch.BLS12_377_Fr_G1_Mont)
 
-    # def test_special(self):
-    #     ##Generate test data
-    #     min_dimension = 5
-    #     # Randomly determine the number of rows and columns
-    #     rows = random.randint(min_dimension, min_dimension)
-    #     columns = random.randint(min_dimension, min_dimension)
-    #     for type_element in data_type:
-    #         random_array_1 = [
-    #             [random.randint(0, 2**64) for _ in range(columns)]
-    #             for _ in range(rows)
-    #         ]
-    #         random_array_2 = [
-    #             [random.randint(0, 2**64) for _ in range(columns)]
-    #             for _ in range(rows)
-    #         ]
-    #         t1 = torch.tensor(random_array_1, dtype=type_element)
-    #         F.to_mont(t1)
+        random_list = [[random.randint(1, 1000) for _ in range(4)] for _ in range(1024)]
+
+        x = torch.tensor(random_list, dtype=torch.BLS12_377_Fr_G1_Base)
+        x_mont = F.to_mont(x)
+
+        x_gpu = x_mont.to("cuda")
+        x_ntt = nttclass.forward(x_gpu)
+        ntt_out = x_ntt.to("cpu")
+        ntt_out= ntt_out.tolist()
+
+        x_intt = inttclass.forward(x_ntt)
+        intt_out = x_intt.to("cpu")
+        intt_out= intt_out.tolist()
+        self.assertEqual(intt_out,x_mont.tolist())
+
+########### Check the correctness of ntt and intt on coset with gpu ###########
+    def test_ntt_coset(self):
+        domain_size = 32
+        nttclass = nn.Ntt_coset(domain_size, torch.BLS12_377_Fr_G1_Mont)
+        inttclass = nn.Intt_coset(domain_size, torch.BLS12_377_Fr_G1_Mont)
+
+        random_list = [[random.randint(1, 1000) for _ in range(4)] for _ in range(1024)]
+
+        x = torch.tensor(random_list, dtype=torch.BLS12_377_Fr_G1_Base)
+        x_mont = F.to_mont(x)
+
+        x_gpu = x_mont.to("cuda")
+        x_ntt = nttclass.forward(x_gpu)
+        ntt_out = x_ntt.to("cpu")
+        ntt_out= ntt_out.tolist()
+
+        x_intt = inttclass.forward(x_ntt)
+        intt_out = x_intt.to("cpu")
+        intt_out= intt_out.tolist()
+        self.assertEqual(intt_out,x_mont.tolist())
+
+
+    def test_special(self):
+        ##Generate test data
+        min_dimension = 5
+        # Randomly determine the number of rows and columns
+        rows = random.randint(min_dimension, min_dimension)
+        columns = random.randint(min_dimension, min_dimension)
+        for type_element in data_type:
+            random_array_1 = [
+                [random.randint(0, 2**64) for _ in range(columns)]
+                for _ in range(rows)
+            ]
+            random_array_2 = [
+                [random.randint(0, 2**64) for _ in range(columns)]
+                for _ in range(rows)
+            ]
+            t1 = torch.tensor(random_array_1, dtype=type_element)
+            F.to_mont(t1)
 
 
 if __name__ == "__main__":

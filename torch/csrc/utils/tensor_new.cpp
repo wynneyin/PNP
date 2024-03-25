a0dd679b16554fc42d5efbcb56fea0b2de0d8f6e
jizhuoran <zrji@sdu.edu.cn>
2024-03-25 10:11:24 +0800

    support empty tensor

--- torch/csrc/utils/tensor_new.cpp
+++ torch/csrc/utils/tensor_new.cpp
@@ -439,7 +439,7 @@ Tensor internal_new_from_data(
       }
     }
     if(c10::isEllipticCurveType(inferred_scalar_type)) {
-      TORCH_CHECK(tensor.size(tensor.dim() - 1) == c10::num_uint64(inferred_scalar_type), 
+      TORCH_CHECK(tensor.size(tensor.dim() - 1) == c10::num_uint64(inferred_scalar_type) || tensor.sizes() == 0, 
         "The last dimension of the tensor must be equal to the number of uint64s in the elliptic curve type.");
     }
     pybind11::gil_scoped_release no_gil;
